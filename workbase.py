5781dceaf821a61232a2eae631dbc8777c37ae9d
xtc007 <110054353+xtc007@users.noreply.github.com>
2022-09-03 09:57:12 -0700

    Update workbase.py

--- workbase.py
+++ workbase.py
@@ -6,11 +6,7 @@ with open("data.txt","w" )as file:
  worker_id=int(input("Enter your worker id"))
 
 data=(f" Your name is:{name} \n Your last name is:{last_name} \n  Your worker id is:{worker_id} \n")
-file.write(name)
-file.write(last_name)
-file.close()
-#file.write(int)(worker_id+"n/")
-#file.close()
+
 
 
 
