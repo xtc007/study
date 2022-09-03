7a7c5352fcf15c3e63cf10e86a9a8268a53f25ff
xtc007 <110054353+xtc007@users.noreply.github.com>
2022-09-03 20:35:42 +0300

    Add files via upload

---
+++ workbase.py
@@ -0,0 +1,22 @@
+
+
+with open("data.txt","w" )as file:
+ name = input("Please enter your name:")
+ last_name = input("Please enter your last name:")
+ worker_id=int(input("Enter your worker id"))
+
+data=(f" Your name is:{name} \n Your last name is:{last_name} \n  Your worker id is:{worker_id} \n")
+#file.write(name)
+#file.write(last_name)
+#file.close()
+#file.write(int)(worker_id+"n/")
+#file.close()
+
+
+
+
+
+
+
+
+
