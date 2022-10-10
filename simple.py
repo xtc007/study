1093ecc005dee8ae88f67aa4dc71e0afc076eb96
xtc007 <110054353+xtc007@users.noreply.github.com>
2022-10-10 11:52:38 -0700

    Update simple.py

--- simple.py
+++ simple.py
@@ -1,7 +1,7 @@
 #!usr/bin/python3
 #Enter your name and last name.
-name=input("Please enter your name:").upper()
-last_name=input("Enter your last name").upper()
+name=input("Please enter your name:")
+last_name=input("Enter your last name")
 #show result:
 print(name,last_name)
 format=print(f"Your name is:{name}",f"Your last name:{last_name} ")
