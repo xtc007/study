04b8314381e3fc3e02e05fbbdbbe99e9fa3247c6
xtc007 <110054353+xtc007@users.noreply.github.com>
2022-11-05 09:38:18 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,14 +1,5 @@
 #!usr/bin/python3
-#Enter your name and last name.
-import time
-name=input("Please enter your name:").upper()
-last_name=input("Enter your last name").upper()
-#show result:
-print(name,last_name)
-format=print(f"Your name is:{name}",f"Your last name:{last_name} ")
 file=open("Simple.txt","w")
-file.write(name)
+file.write("Hello is me")
 file.write("\n")
-file.write(last_name)
 file.close
-time.sleep(60)
